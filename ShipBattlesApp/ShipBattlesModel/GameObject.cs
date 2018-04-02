0cf60809c70a93b453b08d8f7a04271ac50d50ef
jengl352 <jengl352@students.bju.edu>
2018-04-02 18:37:00 -0400

    Serialization

--- ShipBattlesApp/ShipBattlesModel/GameObject.cs
+++ ShipBattlesApp/ShipBattlesModel/GameObject.cs
@@ -9,45 +9,110 @@ namespace ShipBattlesModel
     public abstract class GameObject
     {
         Location Loc { get; set; }
+        Direction Direct { get; set; }
 
+        abstract public void Serialize();
+        abstract public void Deserialize();
+        
     }
 
     public class AIShip: GameObject
     {
-        
+        public override void Serialize()
+        {
+
+        }
+
+        public override void Deserialize()
+        {
+
+        }
     }
 
     public class PlayerShip: GameObject
     {
+        public override void Serialize()
+        {
 
+        }
+
+        public override void Deserialize()
+        {
+
+        }
     }
 
     public class Base: GameObject
     {
+        public override void Serialize()
+        {
 
+        }
+
+        public override void Deserialize()
+        {
+
+        }
     }
 
     public class RepairKit: GameObject
     {
+        public override void Serialize()
+        {
 
+        }
+
+        public override void Deserialize()
+        {
+
+        }
     }
 
     public class Asteroid: GameObject
     {
+        public override void Serialize()
+        {
 
+        }
+
+        public override void Deserialize()
+        {
+
+        }
     }
 
     public class PlayerBullet: GameObject
     {
+        public override void Serialize()
+        {
 
+        }
+
+        public override void Deserialize()
+        {
+
+        }
     }
 
     public class Bullet: GameObject
     {
+        public override void Serialize()
+        {
 
+        }
+
+        public override void Deserialize()
+        {
+
+        }
     }
 
     public class Location
+    {
+
+    }
+
+    public class Direction
     {
 
     }
