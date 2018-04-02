b9fdc10b7a30383d562445b3c1975ffc954c644f
jengl352 <jengl352@students.bju.edu>
2018-04-02 18:42:24 -0400

    Serialization

---
+++ ShipBattlesApp/ShipBattlesModel/GameObject.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipBattlesModel
+{
+    public abstract class GameObject
+    {
+        Location Loc { get; set; }
+        Direction Direct { get; set; }
+
+        abstract public void Serialize();
+        abstract public void Deserialize();
+        
+    }
+
+    public class AIShip: GameObject
+    {
+        public override void Serialize()
+        {
+
+        }
+
+        public override void Deserialize()
+        {
+
+        }
+    }
+
+    public class PlayerShip: GameObject
+    {
+        public override void Serialize()
+        {
+
+        }
+
+        public override void Deserialize()
+        {
+
+        }
+    }
+
+    public class Base: GameObject
+    {
+        public override void Serialize()
+        {
+
+        }
+
+        public override void Deserialize()
+        {
+
+        }
+    }
+
+    public class RepairKit: GameObject
+    {
+        public override void Serialize()
+        {
+
+        }
+
+        public override void Deserialize()
+        {
+
+        }
+    }
+
+    public class Asteroid: GameObject
+    {
+        public override void Serialize()
+        {
+
+        }
+
+        public override void Deserialize()
+        {
+
+        }
+    }
+
+    public class PlayerBullet: GameObject
+    {
+        public override void Serialize()
+        {
+
+        }
+
+        public override void Deserialize()
+        {
+
+        }
+    }
+
+    public class Bullet: GameObject
+    {
+        public override void Serialize()
+        {
+
+        }
+
+        public override void Deserialize()
+        {
+
+        }
+    }
+
+    public class Location
+    {
+
+    }
+
+    public class Direction
+    {
+
+    }
+    
+}
