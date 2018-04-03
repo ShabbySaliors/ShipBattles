4444ba77a08c66facf4d7bf05ff0ae5be4e980a6
zmetz141 <zmetz141@students.bju.edu>
2018-04-02 21:40:08 -0400

    Added HighScore code

---
+++ ShipBattlesApp/ShipBattlesModel/HighScoreTesting.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// Currently does not work.
+namespace ShipBattlesModel
+{
+    [TestClass]
+    public class HighScoreTesting
+    {
+        [TestMethod]
+        public void TestLoadHighScores()
+        {
+            LoadHighScores();
+            Assert.IsTrue(line[9] == "Karen" + " " + "5432");
+        }
+
+        public void TestSaveHighScore()
+        {
+            SaveHighScore("Bob", 9876);
+            Assert.IsTrue(line[0] == "Bob" + " " + "9876");
+        }
+    }
+}
