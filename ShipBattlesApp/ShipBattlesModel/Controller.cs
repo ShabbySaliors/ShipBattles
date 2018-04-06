049b4ff416afed5aeb91846e1bc95118cc353a9c
Jeremiah-England <englandtuning@gmail.com>
2018-04-06 12:50:11 -0400

    AI Ship Logic

--- ShipBattlesApp/ShipBattlesModel/Controller.cs
+++ ShipBattlesApp/ShipBattlesModel/Controller.cs
@@ -14,13 +14,15 @@ namespace ShipBattlesModel
         public GameWorld World { get; set; }
         public int level = 0;
         Random rand = new Random();
-        public double AIShipSpeed = 1;
-        double PlayerSpeed = 2;
+        public int AIShipSpeed = 1;
+        int PlayerSpeed = 2;
 
         public void LoadWorld()
         {
             // Later I will use functions of the 'level' varaible to make the 
             // set up more complicated.
+
+            // Make sure that you set a bullet speed. 
             World.Width = 300;
             World.Height = 300;
             for (int i = 0; i < 5; i++)
@@ -64,7 +66,7 @@ namespace ShipBattlesModel
 
         public Direction MakeRandDirection()
         {
-            return new Direction() { Up = (rand.NextDouble() - 0.5) * 2, Right = (rand.NextDouble() - 0.5) };
+            return new Direction() { Up = (rand.Next(3) - 1) , Right = (rand.Next(3) - 1) };
         }
 
         public Location MakeRandLocation()
--- ShipBattlesApp/ShipBattlesModel/World.cs
+++ ShipBattlesApp/ShipBattlesModel/World.cs
@@ -6,13 +6,15 @@ using System.Threading.Tasks;
 
 namespace ShipBattlesModel
 {
-    public class GameWorld 
+    public class GameWorld
     {
         public Random Rand { get; set; }
         public List<GameObject> Objects { get; set; }
         public int Width { get; set; }
         public int Height { get; set; }
         public PlayerShip PlayerShip { get; set; }
+        public List<GameObject> Plottibles { get; set; }
+        public int BulletSpeed { get; set; }
 
         private GameWorld()
         {
@@ -24,7 +26,24 @@ namespace ShipBattlesModel
             // not be a singleton then? Yes, it must be because we need the objects of the game to check
             // if they are plottable. Of course we could have the controller be a singleton. But we must
             // at least one because the object classes need to reference stuff in the game to make their 
-            // disicions.
+            // disicions. Whatever. I'll just put the World as the singleton and see what happens.
+        }
+
+        public Direction MakeRandomDirection ()
+        {
+            Direction d = new Direction();
+            do
+            {
+                d.Right = Rand.Next(2) - 1;
+                d.Up = Rand.Next(2) - 1;
+            } while (!(d.Right == 0 && d.Up == 0));
+            return d;
+        } 
+
+        private static GameWorld instance = new GameWorld();
+        public static GameWorld Instance
+        {
+            get { return instance; }
         }
     }
 }
