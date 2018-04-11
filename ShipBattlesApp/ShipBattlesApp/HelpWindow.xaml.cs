0b3f8a824913a99b3de11039957cfd93d3b29eb2
metz-coder <zdmetzger@gmail.com>
2018-04-10 21:25:58 -0400

    Built upon HighScore some more

--- ShipBattlesApp/ShipBattlesApp/HelpWindow.xaml.cs
+++ ShipBattlesApp/ShipBattlesApp/HelpWindow.xaml.cs
@@ -23,5 +23,11 @@ namespace ShipBattlesApp
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.MinWidth = 1440.0;
+            this.MinHeight = 900.0;
+        }
     }
 }
